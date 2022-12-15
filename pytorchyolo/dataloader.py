2997ddb48d4e18dd610a9e1f210ee521e7a44884
suchuankai <suchuankai@gmail.com>
2022-12-15 23:44:18 +0800

    add dataloader

---
+++ pytorchyolo/dataloader.py
@@ -0,0 +1,30 @@
+def _create_data_loader(img_path, batch_size, img_size, n_cpu, multiscale_training=False):
+    """Creates a DataLoader for training.
+
+    :param img_path: Path to file containing all paths to training images.
+    :type img_path: str
+    :param batch_size: Size of each image batch
+    :type batch_size: int
+    :param img_size: Size of each image dimension for yolo
+    :type img_size: int
+    :param n_cpu: Number of cpu threads to use during batch generation
+    :type n_cpu: int
+    :param multiscale_training: Scale images to different sizes randomly
+    :type multiscale_training: bool
+    :return: Returns DataLoader
+    :rtype: DataLoader
+    """
+    dataset = ListDataset(
+        img_path,
+        img_size=img_size,
+        multiscale=multiscale_training,
+        transform=AUGMENTATION_TRANSFORMS)
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=n_cpu,
+        pin_memory=True,
+        collate_fn=dataset.collate_fn,
+        worker_init_fn=worker_seed_set)
+    return dataloader
